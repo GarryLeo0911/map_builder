8073d97434ff81470e040ff39a9f955e2f80b531
Garry Li <leo3124894688@gmail.com>
2025-10-27 22:19:36 +1100

    Initiate the project

--- src/map_builder/setup.py
+++ src/map_builder/setup.py
@@ -1,11 +1,13 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'map_builder'
 
 setup(
     name=package_name,
     version='1.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -21,11 +23,6 @@ setup(
     ],
     install_requires=[
         'setuptools',
-        'numpy',
-        'scipy',
-        'scikit-learn',
-        'open3d',
-        'matplotlib',
     ],
     zip_safe=True,
     maintainer='Your Name',
--- src/oakd_driver/setup.py
+++ src/oakd_driver/setup.py
@@ -1,11 +1,13 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'oakd_driver'
 
 setup(
     name=package_name,
     version='1.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -15,9 +17,6 @@ setup(
     ],
     install_requires=[
         'setuptools',
-        'depthai',
-        'opencv-python',
-        'numpy',
     ],
     zip_safe=True,
     maintainer='Your Name',
